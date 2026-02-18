b2292a6f50d5780df24ade8489420f33598dd066
daniilmakarov <nestor123main@gmail.com>
2026-02-18 18:41:51 +0400

    script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.getElementById('nameForm').addEventListener('submit', async (e) => {
 
     try {
         // Отправляем данные на локальный сервер (порт 3000)
-        const response = await fetch('/api/save-name', {
+        const response = await fetch('http://localhost:3000/api/save-name', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -25,5 +25,4 @@ document.getElementById('nameForm').addEventListener('submit', async (e) => {
         console.error('Ошибка:', error);
         messageDiv.innerText = "Сервер не отвечает.";
     }
-
 });
